b9849cc3034529430430a52b123780e18a7fc0e4
abdunzelil <93703261+abdunzelil@users.noreply.github.com>
2022-07-04 16:08:50 +0300

    Update app.js

--- app.js
+++ app.js
@@ -7,3 +7,10 @@ function changeCircleColor(color) {
   const circle = document.querySelector('.titlecircle');
   circle.style.background = color;
 }
+
+function toggleMenu() {
+  var menuToggle = document.querySelector('.toggle');
+  var navigation = document.querySelector('.navigation');
+  menuToggle.classList.toggle('active');
+  navigation.classList.toggle('active');
+}
