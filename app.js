e87006a430b42541f01d56a4ecc123322f1bf90b
abdunzelil <93703261+abdunzelil@users.noreply.github.com>
2024-06-01 15:26:01 +0300

    Update app.js

--- app.js
+++ app.js
@@ -6,8 +6,15 @@ function imgSlider(anything) {
 function changeCircleColor(color) {
   const circle = document.querySelector('.titlecircle');
   circle.style.background = color;
+  const logoSpan = document.querySelector('#logoSpan');
+  logoSpan.style.color = color;
+  const textSpan = document.querySelector('#textSpan');
+  textSpan.style.color = color;
+  const learnMoreButton = document.querySelector('#learnMoreButton');
+  learnMoreButton.style.background = color;
 }
 
+// Güzel bir navbar alternatifi!
 function toggleMenu() {
   var menuToggle = document.querySelector('.toggle');
   var navigation = document.querySelector('.navigation');
